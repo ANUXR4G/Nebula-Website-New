ed71d8e57810e4660f293bff4dd60f817bc112e7
anuxr4g <anun20003@gmail.com>
2024-11-19 18:47:00 +0530

    Initial Commit

--- src/app/nebulatext.jsx
+++ src/app/nebulatext.jsx
@@ -2,12 +2,12 @@
 import React from "react";
 const Nebulatext = () => {
     return (
-      <div className="flex flex-col items-center justify-center h-full bg-white">
-        <h1 className="text-black text-center p-10 font-bold text-[80px] md:text-[100px] leading-none tracking-tight">
+      <div className="flex flex-row items-center justify-center h-full bg-white">
+        <h1 className="text-black text-center p-10 font-bold text-[80px] md:text-[120px] leading-none tracking-tight">
           ( NEBULA &nbsp; ACCELERATOR )
         </h1>
         <p className="text-black text-lg md:text-2xl font-semibold mt-2">
-          EST. 2009
+          EST. 2024
         </p>
       </div>
     );
