62cceb92b437624d4191cb056f488efa2fe129eb
anuxr4g <anun20003@gmail.com>
2024-11-19 18:46:07 +0530

    Initial Commit

---
+++ src/app/nebulatext.jsx
@@ -0,0 +1,17 @@
+"use client"
+import React from "react";
+const Nebulatext = () => {
+    return (
+      <div className="flex flex-col items-center justify-center h-full bg-white">
+        <h1 className="text-black text-center p-10 font-bold text-[80px] md:text-[100px] leading-none tracking-tight">
+          ( NEBULA &nbsp; ACCELERATOR )
+        </h1>
+        <p className="text-black text-lg md:text-2xl font-semibold mt-2">
+          EST. 2009
+        </p>
+      </div>
+    );
+  };
+  
+  export default Nebulatext;
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "@/app/navbar"
+import Nebulatext from "@/app/nebulatext"
 
 function page() {
   return (
     <div>
       <Navbar/>
+      <Nebulatext/>
       </div>
   )
 }
